5e1e7e289e02fbe2560129d3c303bcd313cf4ba2
unknown <bdsayaf@gmail.com>
2023-03-04 20:28:31 +0600

    working on boards feature

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
 import "@/styles/App.scss";
+import '@/styles/globals.css'
 import { AuthContextProvider } from "../context/AuthContext";
 import { ChatContextProvider } from "../context/ChatContext";
 import { SearchContextProvider } from "../context/SearchContext";
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,15 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    mode: "jit",
-    purge: ["./pages/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-      extend: {},
-    },
-    variants: {
-      extend: {},
-    },
-    plugins: [],
-  }
-  
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
